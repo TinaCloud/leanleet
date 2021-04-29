5f1b1d1a11b79b7cc1936a0685b60712796c9e78
TinaCloud <13971635+TinaCloud@users.noreply.github.com>
2021-04-28 20:29:51 -0700

    113

---
+++ 113.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pathSum(self, root: TreeNode, sum):
+        res = []
+        self.helper(root, sum, [], res)
+        return res
+
+    def helper(self, root, sum, path, res):
+
+        if not root:
+            return []
+
+        if not root.left and not root.right and root.val == sum:
+            res.append(path + [root.val])
+
+        self.helper(root.left, sum - root.val, path + [root.val], res)
+        self.helper(root.right, sum - root.val, path + [root.val], res)
+
+
+    
