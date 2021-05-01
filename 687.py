1141f6c2a1c09df1a2ba76657a8a13d7804b22a7
TinaCloud <13971635+TinaCloud@users.noreply.github.com>
2021-05-01 09:30:29 -0700

    687

---
+++ 687.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        self.res = 0
+        self.helper(root, root.val)
+        return self.res
+
+    def helper(self, node, val):
+        if not node:
+            return 0
+
+        left = self.helper(node.left, node.val)
+        right = self.helper(node.right, node.val)
+
+        self.res = max(self.res, left + right)
+        if val == node.val:
+            return max(left, right) + 1
+
+        return 0
