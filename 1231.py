b1d009ee6803a4effe4afedf350242190d2b07d0
TinaCloud <13971635+TinaCloud@users.noreply.github.com>
2021-06-06 10:01:26 -0700

    1231

--- 1231.py
+++ 1231.py
@@ -1,18 +1,27 @@
 class Solution:
     def maximizeSweetness(self, sweetness: List[int], K: int) -> int:
-        left, right = 1, sum(A) / (K + 1)
-        while left < right:
-            mid = (left + right + 1) / 2
-            cur = cuts = 0
-            for a in A:
-                cur += a
-                if cur >= mid:
-                    cuts += 1
-                    cur = 0
-            if cuts > K:
-                left = mid
+        def check(x):
+            s = 0
+            count = 0
+            for num in sweetness:
+                s += num
+                if s >= x:
+                    count += 1
+                    s = 0
+            return count >= K + 1
+
+        l, r = 0, sum(sweetness) // (K + 1)
+        while l < r:
+            mid = l + (r - l + 1) // 2
+            if check(mid):
+                l = mid
             else:
-                right = mid - 1
-        return right
-    
+                r = mid - 1
+        return l
+
+
+
+
+
+
     
